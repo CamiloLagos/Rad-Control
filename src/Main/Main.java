7977a6f41d5265de870f70a81ea7ea116a804c6a
CamiloLagos1 <67348474+CamiloLagos1@users.noreply.github.com>
2020-10-19 18:31:15 -0500

    eliminacion de clases obsoletas en el main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -25,8 +25,6 @@ public class Main {
     public static void main(String[] args) {
         try {
             Control control = new Control();           
-            control.agregarUsuario(new Usuario("admin", "1"));
-            control.agregarUsuario(new Usuario("Camilo", "123"));
             control.agregarVoltaje(new Voltaje(50, new Material(8.801, 2.73E+01, 0.2957), new Material(0.09032, 0.1712, 0.2324), new Material(1.817, 4.84, 0.4021)));
             control.agregarVoltaje(new Voltaje(70, new Material(5.369, 23.49, 0.5881), new Material(0.05087, 0.1696, 0.3847), new Material(0.7149, 3.798, 0.5378)));
             control.agregarVoltaje(new Voltaje(90, new Material(3.067, 18.83, 0.7726), new Material(0.04228, 0.1137, 0.469), new Material(0.3971, 2.913, 0.7204)));
